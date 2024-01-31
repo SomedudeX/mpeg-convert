7e3b2d4b3588bee57b299ceb6fe7cfc84a93402b
SomedudeX <101906945+SomedudeX@users.noreply.github.com>
2024-01-30 17:03:06 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,15 @@
-echo "Receiving dependencies for mpeg-convert"
+echo " - Receiving dependencies for mpeg-convert"
 curl -s https://raw.githubusercontent.com/SomedudeX/MPEG-Convert/v1.0.0/requirements.txt > requirements.txt
 
-echo "Installing dependencies with pip3: \n - python-ffmpeg\n - rich"
-pip3 install -r requirements.txt
+echo " - Installing dependencies with pip3: \n   * python-ffmpeg\n   * rich"
+python -m pip install -r requirements.txt
 
-echo "Receiving script for mpeg-convert"
-curl -s https://raw.githubusercontent.com/SomedudeX/MPEG-Convert/v1.1.0/App/mpeg-convert.py > mpeg-convert.py
+echo " - Receiving script for mpeg-convert"
+curl -s https://raw.githubusercontent.com/SomedudeX/MPEG-Convert/main/App/mpeg-convert.py > mpeg-convert.py
+echo " - Received mpeg-convert.py"
 
+echo " - Purging files"
 rm requirements.txt
 
-echo "Succesfully installed mpeg-convert.py"
-echo "You may want to chmod +x the file"
+echo " - Notes: \n       You may want to 'sudo chmod +x' the file"
+echo " - Installation script finished"
